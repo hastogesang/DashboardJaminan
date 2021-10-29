55703502d9d5eb5c06f7ab7bdd8d77300a2ecebe
markuskurniawan <markuskurniawan78@gmail.com>
2021-10-29 14:19:57 +0700

    membuat tampilan report jaminan

---
+++ src/main/java/com/dashboard/controller/ReportJaminanController.java
@@ -0,0 +1,16 @@
+package com.dashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/reportjaminan")
+public class ReportJaminanController {
+    @GetMapping("") 
+    public ModelAndView index(){
+        ModelAndView view = new ModelAndView("reportjaminan/index");
+        return view;
+    }
+}
---
+++ src/main/resources/static/js/reportjaminan.js
@@ -0,0 +1,53 @@
+function TampilData(){
+    let form = '';
+    form += `
+        <form>
+            <div class="row">
+              <div class="form-group form-row col-md-6">
+                <label for="namaReport" class="col-sm-4 col-form-label">Nama Report</label>
+                <div class="col-sm-8">
+                  <select id="namaReport" class="form-control form-select-sm">
+                    <option selected>Report Dana Jaminan</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+            <div class="form-group form-row col-md-6">
+                <label for="anggotaKeliring" class="col-sm-4 col-form-label">Nama Anggota Kliring</label>
+                <div class="col-sm-8">
+                <select id="anggotaKeliring" class="form-control form-select-sm">
+                    <option selected>PT Agrodana Futures</option>
+                    <option>PT Asia Trade Point Futures</option>
+                    <option>PT Askap Futures</option>
+                </select>
+                </div>
+            </div>
+            </div>
+
+            <div class="row">
+              <div class="form-group form-row col-md-6">
+                <label for="tanggalAwal" class="col-sm-4 col-form-label">Tanggal Jatuh Tempo</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="tanggalAwal" placeholder="dd/mm/yy" required>
+                </div>
+              </div>
+              <div class="form-group form-row col-md-4">
+                <label for="tanggalAkhir" class="col-sm-4 col-form-label">Sampai Dengan</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="tanggalAkhir" placeholder="dd/mm/yy" required>
+                </div>
+              </div>
+            </div>
+            
+            <button type="submit" class="btn btn-sm btn-primary">Generate single report</button>
+            <button type="submit" class="btn btn-sm btn-primary">Generate all report</button>
+        </form>
+    `;
+    $("#content").html(form);
+}
+
+$(document).ready(function() {
+    TampilData();
+})
